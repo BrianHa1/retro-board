4060a5f859d50ac279a75bd44d2945b3cf911239
Brian Ha <brianha32@gmail.com>
2020-03-11 20:46:27 -0400

    Need to get categories to line up properly.

---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Categories(){
+    return (
+        <div>
+            <h2 align="left">Went Well</h2>
+            <h2 align="center">To Improve</h2>
+            <h2 align="right">Action Items</h2>
+        </div>
+    );
+}
+
+export default Categories;
