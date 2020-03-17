a746a1c8ff221b9838c22c2a2b3e6037142a62cc
Brian Ha <brianha32@gmail.com>
2020-03-16 21:50:42 -0400

    Got headers to line up on one line. Except for the one in the center, which is positioned a little higher.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,9 +3,9 @@ import React from "react";
 function Categories(){
     return (
         <div>
-            <h2 align="left">Went Well</h2>
-            <h2 align="center">To Improve</h2>
-            <h2 align="right">Action Items</h2>
+            <h2 style={{display: "inline", float: "left"}}>Went Well</h2>
+            <h2 style={{display: "inline", float: "center" }}>To Improve</h2>
+            <h2 style={{display: "inline", float: "right"}}>Action Items</h2>
         </div>
     );
 }
