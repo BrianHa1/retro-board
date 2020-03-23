60d5e3a0090e35947c1507eae820f88183afdcec
Brian Ha <brianha32@gmail.com>
2020-03-22 22:18:54 -0400

    Another commit.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,11 +2,40 @@ import React from "react";
 
 function Categories(){
     // This will display all the categories for the retro board.
+    // Each category is put into as a header.
+    // The respective headers are aligned to the far left, in the center, and to the far right.
     return (
         <div>
-            <h2 style={{display: "inline", float: "left"}}>Went Well</h2>
-            <h2 style={{display: "inline", float: "center" }}>To Improve</h2>
-            <h2 style={{display: "inline", float: "right"}}>Action Items</h2>
+            <h2 style={{display: "inline", float: "left"}}>
+                Went Well
+                <button>+</button>
+                <input type="text" placeholder="Enter text here" />
+                <ul><button>Move left</button></ul>
+                <ul><button>Remove</button></ul>
+                <ul><button>Move right</button></ul>
+                <ul><button>Like</button></ul>
+                <ul><button>Dislike</button></ul>
+            </h2>
+            <h2 style={{display: "inline", float: "center" }}>
+                To Improve
+                <button>+</button>
+                <input type="text" placeholder="Enter text here" />
+                <ul><button>Move left</button></ul>
+                <ul><button>Remove</button></ul>
+                <ul><button>Move right</button></ul>
+                <ul><button>Like</button></ul>
+                <ul><button>Dislike</button></ul>
+            </h2>
+            <h2 style={{display: "inline", float: "right"}}>
+                Action Items
+                <button>+</button>
+                <input type="text" placeholder="Enter text here" />
+                <ul><button>Move left</button></ul>
+                <ul><button>Remove</button></ul>
+                <ul><button>Move right</button></ul>
+                <ul><button>Like</button></ul>
+                <ul><button>Dislike</button></ul>
+            </h2>
         </div>
     );
 }
